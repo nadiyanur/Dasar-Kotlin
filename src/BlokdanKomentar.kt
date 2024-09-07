53683732b07f5aa93abc10feb5025b8cde5e6bf6
Nadiya <diyay258@gmail.com>
2024-09-07 08:15:59 +0700

    upload stater project

---
+++ src/BlokdanKomentar.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    println("Hello, ini program pertama saya di kotlin")
+
+    //Contoh komentar 1 baris
+    /*
+    contoh komentar multi baris
+     */
+
+    /**
+     * kotlin Doc --> baris ini untuk mendokumentasikan kode program
+     */
+}
+
